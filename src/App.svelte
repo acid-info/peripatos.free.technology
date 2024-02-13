49898fdaf4e41183ffae21b14c96fc2a03087bf7
jinhojang6 <jinho5051@gmail.com>
2024-02-13 20:57:33 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import Ps1 from "./components/Ps1.svelte";
+  import Input from "./components/Input.svelte";
+  import History from "./components/History.svelte";
+  import { theme } from "./stores/theme";
+</script>
+
+<svelte:head>
+  {#if import.meta.env.VITE_TRACKING_ENABLED === "true"}
+    <script
+      async
+      defer
+      data-website-id={import.meta.env.VITE_TRACKING_SITE_ID}
+      src={import.meta.env.VITE_TRACKING_URL}
+    ></script>
+  {/if}
+</svelte:head>
+
+<main
+  class="h-full p-4 overflow-auto text-xs border-2 rounded-md sm:text-sm md:text-base"
+  style={`background-color: ${$theme.background}; color: ${$theme.foreground}; border-color: ${$theme.green};`}
+>
+
+  <History />
+
+  <div class="lex-col md:flex-row">
+    <Ps1 />
+
+    <Input />
+
+    <div class="buttons-row">{`
+----------------------------------------------------------------------------
+                        < Apply >           < X >
+-----------------------------------------------------------------------------
+`}
+    </div>
+  </div>
+</main>
---
+++ src/components/Ps1.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { theme } from "../stores/theme";
+  let hostname = window.location.hostname;
+</script>
+
+<h1 class="flex font-bold">
+  <span style={`color: ${$theme.yellow};`}>guest</span>
+  <span style={`color: ${$theme.white}`}>@</span>
+  <span style={`color: ${$theme.green}`}>{hostname}</span>
+  <span style={`color: ${$theme.white}`}>:~$</span>
+</h1>
